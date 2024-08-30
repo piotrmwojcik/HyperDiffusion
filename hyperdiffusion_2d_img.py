00ee9fe4554de016f9a71cec2de657a5fae21fd9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-30 14:03:43 +0200

    fix

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -144,7 +144,9 @@ class HyperDiffusion_2d_img(pl.LightningModule):
         model_input = {'coords': model_input}
         result = siren(model_input)
         img = result['model_out'][0].view(128, 128, 3)
-        self.logger.log_image("val", img)
+        images = wandb.Image(img, caption="")
+        wandb.log({"examples": images})
+        #self.logger.log_image("val", img)
         #metric_fn = (
         #    self.calc_metrics_4d
         #    if self.cfg.mlp_config.params.move
