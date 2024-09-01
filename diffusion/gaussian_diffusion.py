9c0b595c9e1f3afd90ca0673dfad85d3adbcc6ba
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-01 12:21:29 +0200

    added modulation

--- diffusion/gaussian_diffusion.py
+++ diffusion/gaussian_diffusion.py
@@ -820,22 +820,8 @@ class GaussianDiffusion:
                 ModelMeanType.EPSILON: noise,
             }[self.model_mean_type]
             assert model_output.shape == target.shape == x_start.shape
-            mse1 = torch.zeros(x_t.shape[0]).cuda()
-
-            import torch.nn.functional as F
-            for i in range(target.shape[0]):
-                siren_target = generate_mlp_from_weights(target[i], mlp_kwargs).cuda()
-                siren_output = generate_mlp_from_weights(model_output[i], mlp_kwargs).cuda()
-                model_input = get_mgrid(128, 2).unsqueeze(0).cuda()
-                model_input = {'coords': model_input}
-                with module_requires_grad(siren_target, False):
-                    result_target = siren_target(model_input)['model_out'][0]
-                    with module_requires_grad(siren_output, False):
-                        result_output = siren_output(model_input)['model_out'][0]
-                        mse2 = F.mse_loss(result_output, result_target)
-                        mse1[i] += mse2
-            terms["mse"] = mse1
-
+            # mlp = MLP(**mlp_kwargs)
+            terms["mse"] = mean_flat((target - model_output) ** 2)
             if "vb" in terms:
                 terms["loss"] = terms["mse"] + terms["vb"]
             else:
