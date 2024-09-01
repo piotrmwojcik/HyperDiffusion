c2a6392dbd51f79b53faf938559be94f48aa5252
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-01 12:25:53 +0200

    added modulation

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -112,7 +112,7 @@ class HyperDiffusion_2d_img(pl.LightningModule):
             #print("Input images shape:", input_data.shape)
 
         # Output statistics every 100 step
-        if self.trainer.global_step % 100 == 0:
+        if self.trainer.global_step % 10 == 0:
             print(input_data.shape)
             print(
                 "Orig weights[0].stats",
@@ -170,9 +170,9 @@ class HyperDiffusion_2d_img(pl.LightningModule):
         model_input = {'coords': model_input}
         result = siren(model_input)
         img = result['model_out'][0].view(1, 128, 128, 3)
-        print(img[0, :, :, 0])
-        print('!!!')
-        print(img[0, :, :, 1])
+        #print(img[0, :, :, 0])
+        #print('!!!')
+        #print(img[0, :, :, 1])
         img_min = img.min()
         img_max = img.max()
         img = (img - img_min) / (img_max - img_min)
