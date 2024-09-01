746a452a06484c486bd7ef79faa60dd237e97d6a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-01 20:12:53 +0200

    added modulation

--- simple_test2.py
+++ simple_test2.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
             all_weights.append(weights)
         all_weights = torch.cat(all_weights, dim=0)
         from sklearn.decomposition import PCA
-        pca = PCA(n_components=400)  # You can choose the number of components
+        pca = PCA(n_components=1000)  # You can choose the number of components
         pca_result = torch.tensor(pca.fit_transform(all_weights))
         basis = torch.tensor(pca.components_)
         #print(ca_result.shape)
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         std = torch.std(pca_result, dim=0)  # Shape: (400,)
 
         # Draw new coefficients from a normal distribution with the computed mean and std
-        coefficients = 1.0*pca_result[5]# + 0.3*pca_result[10]#torch.normal(mean, std)  # Shape: (400,)
+        coefficients = pca_result[5]# + 0.3*pca_result[10]#torch.normal(mean, std)  # Shape: (400,)
 
         # coefficients will have the same shape as one PCA component vector (400,)
         print("Mean:", mean)
