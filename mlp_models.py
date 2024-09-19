4944023bfef9c21eed2e5718c42e1db040d5be02
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-19 11:39:18 +0200

    added implicit mlp

--- mlp_models.py
+++ mlp_models.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn.functional as F
 from torch import distributions as dist
 from torch import nn
+import math
+import torch.nn.functional as F
 
 from embedder import Embedder
 
