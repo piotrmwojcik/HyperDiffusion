951047f56c58ea4f57d1f6ec55e5fa39848e042c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-22 12:02:14 +0200

    added loop

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -92,6 +92,8 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             # print(img.shape)
             # images = wandb.Image(img, caption="")
             # wandb.log({"examples": images})
+            print('!!!')
+            print(global_step)
             self.logger.log({"train": [img]})
             #sdf_decoder = SDFDecoder(
             #    self.mlp_kwargs.model_type,
