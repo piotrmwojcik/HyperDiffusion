1564f677b79d94f199d837bca84cc930fd3b3163
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-09-30 19:46:47 +0200

    added cache

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -190,9 +190,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         log_interval = int(Config.get("log_interval"))
 
-        #if 'code_optimizer' in self.train_cfg:
-        #    code_list_, code_optimizers = self.load_cache(train_batch)
-        #    code = torch.stack(code_list_, dim=0)
+        if 'code_optimizer' in self.train_cfg:
+            code_list_, code_optimizers = self.load_cache(train_batch)
+            code = torch.stack(code_list_, dim=0)
 
         # At the first step output first element in the dataset as a sanit check
         if "hyper" in self.method and global_step % 50 == 0 and global_step % log_interval == 0:
