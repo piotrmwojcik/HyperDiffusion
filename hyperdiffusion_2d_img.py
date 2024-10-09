7bd2a3bf5ba934d4b25ea8e70cc98e77b81dfffe
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-09 21:33:51 +0200

    save cache

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -326,9 +326,8 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         global_step += 1
         loss_mse.backward()  # Backpropagation
         optimizer.step()
-
-        print(code.grad.data())
         print(loss_terms["loss"].grad.data)
+        print(code.grad.data())
         prior_grad = [code_.grad.data.clone() for code_ in code_list_]
 
         self.logger.log({"global_step": global_step, "train_loss": loss_mse})
