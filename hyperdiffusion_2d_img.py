018883e0f855dd5525d03e9bf69c374d4023f03c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-11 19:23:27 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -272,12 +272,18 @@ class HyperDiffusion_2d_img(torch.nn.Module):
     def inverse_code(self, gt_imgs, grids, code_, code_optimizer, prior_grad, cfg):
         n_inverse_steps = cfg['inverse_steps']
 
+        mlps = [None for _ in code_]
+
         for inverse_step_id in range(n_inverse_steps):
             mse_loss = []
 
             for code_idx, code_single in enumerate(code_):
 
-                mlp = generate_mlp_from_weights_trainable(code_single, self.mlp_kwargs)
+                if mlps[code_idx] is None:
+                    mlp = generate_mlp_from_weights_trainable(code_single, self.mlp_kwargs)
+                    mlps[code_idx] = mlp
+                else:
+                    mlp = mlps[code_idx]
                 output = mlp({'coords': grids[code_idx].unsqueeze(0)})
 
                 loss = image_mse(mask=None, model_output=output, gt=gt_imgs[code_idx].unsqueeze(0))
