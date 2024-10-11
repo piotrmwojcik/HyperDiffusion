8ea8e8ea7d721e2281aa2cb531d5b4a65431163e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-11 10:31:43 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -286,9 +286,10 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 loss = image_mse(mask=None, model_output=output, gt=gt_imgs)
                 code_[code_idx].grad.copy_(prior_grad[code_idx])
                 code_optimizer[code_idx].step()
+
+                code_optimizer[code_idx].zero_grad()
+
                 loss.backward()
-
-
 
     def training_step(self, train_batch, optimizer, global_step):
         # Extract input_data (either voxel or weight) which is the first element of the tuple
