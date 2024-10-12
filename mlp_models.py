ea3458149a15e39b1e13049ec146720888a7820c
kacperunia <kacpermarzol@gmail.com>
2024-10-12 20:25:09 +0200

    scaling

--- mlp_models.py
+++ mlp_models.py
@@ -95,6 +95,7 @@ class FMMLinear(nn.Module):
 
     def forward(self, input):
         W = self.left_matrix @ self.right_matrix # [batch_size, out_channel, in_channel]
+        W /= np.sqrt(self.rank)
         out = F.linear(input, W, self.bias)
 
         return out
