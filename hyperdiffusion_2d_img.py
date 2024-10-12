264061e411c280ab1806e27d90e8108cfed89500
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-12 18:42:24 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -284,7 +284,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
                 code_optimizer[code_idx].zero_grad()
                 loss['img_loss'].backward()
-                print(loss['img_loss'].item())
+                #print(loss['img_loss'].item())
 
                 #print(inverse_step_id, code_idx, loss['img_loss'].item())
                 code_optimizer[code_idx].step()
