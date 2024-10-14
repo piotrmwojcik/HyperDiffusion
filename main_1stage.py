ec72d1f83b1fbda8a59e376f11249874e52c22e7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-14 22:51:42 +0200

    inverse_code

--- main_1stage.py
+++ main_1stage.py
@@ -245,7 +245,7 @@ def main(cfg: DictConfig):
                 #total_train_loss = 0.0
                 for batch_idx, data in enumerate(train_dl):
                     #data = data.to(device)
-                    print(batch_idx, len(train_dl))
+                    print(batch_idx, len(train_dl), global_step)
 
                     optimizer.zero_grad()  # Zero gradients
 
