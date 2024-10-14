db3ab3369c1c7d1e952a9ccb4d6c7b6e7db5d2fb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-14 14:51:50 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -282,13 +282,15 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 #   print(code_single)
                 mlp = mlps[code_idx]
                 mlp_params = [param for name, param in mlp.named_parameters()]
-                input = grids[code_idx].unsqueeze(0).requires_grad_()
+                input = grids[code_idx].unsqueeze(0)
                 output = mlp({'coords': input})
 
                 loss_inner = image_mse(mask=None, model_output=output, gt=gt_imgs[code_idx].unsqueeze(0))
                 grad_inner = torch.autograd.grad(loss_inner['img_loss'],
-                                                 input,
+                                                 mlp_params,
                                                  create_graph=False)
+                for i, grad in enumerate(grad_inner):
+                    print(f"Gradient for parameter {i}: {grad}")
                 for p_idx, params in enumerate(mlp_params):
                     print(grad_inner[p_idx])
                     mlp_params[p_idx] = params - cfg['code_lr'] * grad_inner[p_idx]
