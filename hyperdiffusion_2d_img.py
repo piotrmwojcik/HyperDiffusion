17ad5866d89f6dced5e5dbb0e2e4a058d8a693f7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-14 20:05:45 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -300,13 +300,13 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                         param -= cfg['code_lr'] * grad
                         current_idx += num_params
                 #print(loss_inner['img_loss'].item())
-         for single_code, mlp in zip(code_, mlps):
-             state_dict = mlp.state_dict()
-             weights = []
-             for weight in state_dict:
-                 weights.append(state_dict[weight].flatten().cpu())
-             input_data = torch.hstack(weights)
-             print(input_data.grad.data)
+        for single_code, mlp in zip(code_, mlps):
+            state_dict = mlp.state_dict()
+            weights = []
+            for weight in state_dict:
+                weights.append(state_dict[weight].flatten().cpu())
+            input_data = torch.hstack(weights)
+            print(input_data.grad.data)
 
 
     def training_step(self, train_batch, optimizer, global_step):
