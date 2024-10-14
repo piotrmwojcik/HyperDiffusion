42a930f27e893233106ee516d10cc430fdd86039
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-14 18:49:45 +0200

    inverse_code

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -299,6 +299,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                         grad = grad + prior_grad[code_idx][current_idx:current_idx + num_params]
                         grad.view(grad_shape)
                         param -= cfg['code_lr'] * grad
+                        current_idx += num_params
                 print(loss_inner['img_loss'].item())
             print()
 
