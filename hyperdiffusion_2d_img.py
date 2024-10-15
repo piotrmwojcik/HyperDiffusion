d74ba5eb907773d1b6b00c3516187ef38a6bddb8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-15 14:55:50 +0200

    validation image

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -275,9 +275,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         mlps = [generate_mlp_from_weights(code_single, self.mlp_kwargs) for code_single in code_]
         for code_idx, _ in enumerate(code_):
+            code_optimizer[code_idx].param_groups[0]['params'] = mlps[code_idx].parameters()
             print('!!!!!!')
             print(code_optimizer[code_idx].state_dict())
-            code_optimizer[code_idx].param_groups[0]['params'] = mlps[code_idx].parameters()
         mse_loss = []
         for inverse_step_id in range(n_inverse_steps):
 
