051fcfc723ca2a87b5348fbf9306fbbe167b112d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-20 07:16:02 +0200

    scheduler

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -358,7 +358,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         for code_optimizer in code_optimizers:
             code_optimizer.zero_grad()
-            if 'step' in code_optimizer.state_dict()['state']:
+            if 'step' in code_optimizer.state_dict()['state'][0]:
                 print(code_optimizer.state_dict()['state'][0]['step'],
                       code_optimizer.state_dict()['state'][1]['step'])
             else:
