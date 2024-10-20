edfd9ba206029e6f0829e9530e4f9ec97d922f21
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-20 07:31:02 +0200

    scheduler

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -363,7 +363,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 print(code_optimizer.state_dict()['state']['0']['step'],
                       code_optimizer.state_dict()['state']['1']['step'])
             else:
-                print(code_optimizer.state_dict()['state']['state'].keys())
+                print(code_optimizer.state_dict()['state'].keys())
+                if 'state' in code_optimizer.state_dict()['state']:
+                    print('dupa')
 
         #start = time.time()
         for inverse_step_id in range(n_inverse_steps):
