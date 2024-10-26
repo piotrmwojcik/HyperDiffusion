c40943742c11ee5664b06c337a0547e705c12d97
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-26 20:44:30 +0200

    fix

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -118,7 +118,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         optimizer.__setstate__({'state': state})
 
     def get_init_code_(self, device=None):
-        model = ImplicitMLP(B_path=self.mlp_kwargs['B_path'])
+        model = ImplicitMLP(B=self.loaded_B)
         #checkpoint_path = "/data/pwojcik/siren/logs/058462.jpg/checkpoints/model_epoch_14500.pth"
         #checkpoint = torch.load(checkpoint_path, map_location=device)
         #model.load_state_dict(checkpoint)
