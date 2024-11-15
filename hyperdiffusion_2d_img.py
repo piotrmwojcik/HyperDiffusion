25d74b94a171db91cec4af56ec5100b48bc4f9f7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-15 21:41:23 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -388,10 +388,11 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             #mlp_params = [param for name, param in mlp.named_parameters()]
             output = mlp({'coords': grids})
             #start = time.time()
-            loss_inner = image_mse(mask=None, model_output=output, gt=gt_imgs)['img_loss']
-            mse_loss = loss_inner * Config.get('code_loss_weight')
             print('!!!')
             print(gt_imgs.shape)
+            loss_inner = image_mse(mask=None, model_output=output, gt=gt_imgs)['img_loss']
+            mse_loss = loss_inner * Config.get('code_loss_weight')
+
             psnr_inner = image_psnr(output['model_out'], gt_imgs)['img_psnr']
             psnr.append(psnr_inner)
 
