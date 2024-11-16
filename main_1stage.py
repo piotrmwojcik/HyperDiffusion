32a671f34c62addf45b805412843f906d68ab4d4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-16 22:59:48 +0100

    parallel

--- main_1stage.py
+++ main_1stage.py
@@ -301,7 +301,7 @@ def main(cfg: DictConfig):
                     for p in code_loss_weight_schedule:
                         if epoch >= p[0]:
                             diffuser.cfg['code_loss_weight'] = p[1]
-                    loss, code_optimizer_state_ = diffuser.training_step(data, optimizer, None, global_step, save_to_disk)  # Forward pass
+                    loss, code_optimizer_state_ = diffuser.training_step(data, optimizer, code_optimizer_state, global_step, save_to_disk)  # Forward pass
                     outputs.append(loss)
                     global_step += 1
                     pbar.set_postfix({"diff_loss": loss.item()})
