73ea4f124281344e19f09079cce74813ad4df77c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-17 15:57:12 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -369,7 +369,6 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             optim = code_optimizer.state_dict()
             optim['state'] = optimizer_state['state']
             code_optimizer.load_state_dict(optim)
-        print(code_optimizer.state_dict()['state'].keys())
         code_optimizer.zero_grad()
 
         if n_inverse_steps == 0:
@@ -424,6 +423,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             code_[idx] = torch.hstack(weights)
 
         psnr = torch.mean(torch.hstack(psnr))
+        print(code_optimizer.state_dict()['state'][0]['step'])
         optimizer_state_ = self.optimizer_state_to(code_optimizer.state_dict(), device='cpu', dtype=torch.float32)
         return mse_loss, psnr, optimizer_state_
 
