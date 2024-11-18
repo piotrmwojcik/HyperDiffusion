7fa4ce5275679bde190684ce66d8ba4465dc69f5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-19 00:18:02 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -308,6 +308,8 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         else:
             update_grad = True
 
+        psnr = None
+        mse_loss = None
         #start = time.time()
         for inverse_step_id in range(n_inverse_steps):
             #psnr = []
@@ -427,9 +429,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             input = train_batch['coords'][0].unsqueeze(0)
             inr_output = mlp({'coords': input})['model_out'][0].view(64, 64, 3).permute(2, 0, 1)
 
-            psnr = image_psnr(mlp({'coords': input})['model_out'], train_batch['gt_img'][0].unsqueeze(0))['img_psnr']
+            psnr_test = image_psnr(mlp({'coords': input})['model_out'], train_batch['gt_img'][0].unsqueeze(0))['img_psnr']
             print('!!!!')
-            print(psnr)
+            print(psnr_test)
 
             images = wandb.Image(input_img, caption="")
             inr_images = wandb.Image(inr_output, caption="")
