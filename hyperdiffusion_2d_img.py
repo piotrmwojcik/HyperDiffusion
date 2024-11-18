eee799a32ceb0d16e639efa6bf3a2d9d60eff280
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-19 00:33:16 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -371,7 +371,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
     def training_step(self, train_batch, optimizer, code_optimizer_state, global_step, save_to_disk):
         # Extract input_data (either voxel or weight) which is the first element of the tuple
-        input_img = train_batch['gt_img'][0].view(64, 64, 3).permute(2, 0, 1).cuda()
+        input_img = train_batch['gt_img'][16].view(64, 64, 3).permute(2, 0, 1).cuda()
 
         log_interval = int(Config.get("log_interval"))
 
