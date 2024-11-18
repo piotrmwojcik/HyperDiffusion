865dba2dc83ccbdab1873be13352ec6f65113c1d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-18 17:33:59 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -399,18 +399,18 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 #end_grad = time.time()
                 #print(f"grad inner step took {round(end_grad - start, 3)} seconds")
 
-                for code_idx, single_mlp in enumerate(mlp.models):
-                    prior_grad[code_idx] = prior_grad[code_idx].cuda()
-                    current_idx = 0
-                    for grad, param in zip(grad_inner, single_mlp.parameters()):
-                        grad_shape = grad.shape
-                        num_params = np.product(list(grad.shape))
-                        grad = grad.view(-1)
-                        grad = grad + prior_grad[code_idx][current_idx:current_idx + num_params]
-                        grad = grad.view(grad_shape)
-                        param.grad = torch.zeros_like(param).cuda()
-                        current_idx += num_params
-                        param.grad.copy_(grad)
+                # for code_idx, single_mlp in enumerate(mlp.models):
+                #     prior_grad[code_idx] = prior_grad[code_idx].cuda()
+                #     current_idx = 0
+                #     for grad, param in zip(grad_inner, single_mlp.parameters()):
+                #         grad_shape = grad.shape
+                #         num_params = np.product(list(grad.shape))
+                #         grad = grad.view(-1)
+                #         grad = grad + prior_grad[code_idx][current_idx:current_idx + num_params]
+                #         grad = grad.view(grad_shape)
+                #         param.grad = torch.zeros_like(param).cuda()
+                #         current_idx += num_params
+                #         param.grad.copy_(grad)
 
                 code_optimizer.step()
         #end = time.time()
