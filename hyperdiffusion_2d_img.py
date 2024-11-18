70c13aaab82c3345f19cef6954adac254cd9b007
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-19 00:24:37 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -308,16 +308,12 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         else:
             update_grad = True
 
-        psnr = None
-        mse_loss = None
         #start = time.time()
         for inverse_step_id in range(n_inverse_steps):
             #psnr = []
             output = mlp(grids)
             #print(output['model_out'].shape)
             #start = time.time()
-            psnr = None
-            mse_loss = None
             mse_loss = image_mse(mask=None, model_output=output, gt=gt_imgs)['img_loss']
             mse_loss = mse_loss * Config.get('code_loss_weight')
 
