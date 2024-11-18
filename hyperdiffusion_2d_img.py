4430de33c7dd74fba578348218df201064a32820
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-18 16:27:50 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -488,10 +488,10 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         #print('before inverse code')
         #start = time.time()
-        code_optimizer_state_ = self.deep_copy_dict(code_optimizer_state)
-        inv_loss, psnr, optim_state = self.inverse_code_1b1(train_batch['gt_img'], train_batch['coords'], code_list_, code_optimizer_state_,
+        #code_optimizer_state_ = self.deep_copy_dict(code_optimizer_state)
+        inv_loss, psnr, code_optimizer_state_  = self.inverse_code_1b1(train_batch['gt_img'], train_batch['coords'], code_list_, code_optimizer_state,
                                                             prior_grad, self.cfg)
-        code_optimizer_state_ = self.deep_copy_dict(optim_state)
+        #code_optimizer_state_ = self.deep_copy_dict(optim_state)
 
         if "hyper" in self.method and global_step % 50 == 0 and global_step % log_interval == 0:
             mlp = generate_mlp_from_weights(code_list_[0], self.mlp_kwargs, self.loaded_B)
