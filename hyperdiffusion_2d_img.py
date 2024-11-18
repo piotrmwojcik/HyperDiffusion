cfc2d660204715fd9e65a46be90f6b6a3e249402
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-18 23:31:16 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -317,6 +317,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             mse_loss = image_mse(mask=None, model_output=output, gt=gt_imgs)['img_loss']
             mse_loss = mse_loss * Config.get('code_loss_weight')
             #print(mse_loss)
+            print(output['model_out'].shape, gt_imgs.shape)
             psnr = image_psnr(output['model_out'], gt_imgs)['img_psnr']
             #psnr.append(psnr_inner)
 
