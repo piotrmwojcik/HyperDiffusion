f349449b1b78fd8b0f091f4b9b5a6c74432ff930
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-18 21:15:06 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -332,6 +332,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                         grad_shape = grad.shape
                         num_params = np.product(list(grad.shape))
                         grad = grad.view(-1)
+                        print(grad)
                         grad = grad #+ prior_grad[code_idx][current_idx:current_idx + num_params]
                         grad = grad.view(grad_shape)
                         param.grad = torch.zeros_like(param).cuda()
