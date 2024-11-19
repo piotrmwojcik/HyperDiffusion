a2adbada886c0305582e3ab47c06b9772617dbb0
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-19 09:01:12 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -325,13 +325,13 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                                                  mlp.parameters(),
                                                  create_graph=False)
 
-                print('!!!')
-                print(len(grad_inner), len(list(mlp.parameters())))
+                #print('!!!')
+                #print(len(grad_inner), len(list(mlp.parameters())))
 
                 #for code_idx, single_mlp in enumerate(mlp.models):
                 #    prior_grad[code_idx] = prior_grad[code_idx].cuda()
                 #    current_idx = 0
-                for grad, param in zip(grad_inner, mlp.parameters().parameters()):
+                for grad, param in zip(grad_inner, mlp.parameters()):
                     grad_shape = grad.shape
                     num_params = np.product(list(grad.shape))
                     grad = grad.view(-1)
