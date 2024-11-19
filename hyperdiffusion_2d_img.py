00b057bf223150d6acdecc3c1ca5c7fda5a9c5dd
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-19 02:07:07 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -322,7 +322,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
             if update_grad:
                 grad_inner = torch.autograd.grad(mse_loss,
-                                                 list(mlp.parameters()),
+                                                 mlp.parameters(),
                                                  create_graph=False)
 
 
