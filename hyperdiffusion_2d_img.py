24200fa774eaa3302e2516ecaf38db74bc7f3a6b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-20 11:52:50 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -122,7 +122,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
     def get_init_code_(self, device=None):
         #print('!!!!')
-        #model = ImplicitMLP(B=self.loaded_B)
+        model = ImplicitMLP(B=self.loaded_B)
         #checkpoint_path = "/data/pwojcik/siren/logs/033013.jpg/checkpoints/model_epoch_14500.pth"
         #checkpoint = torch.load(checkpoint_path, map_location=device)
         #model.load_state_dict(checkpoint)
