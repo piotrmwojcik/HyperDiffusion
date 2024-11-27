20e660bb240bd982f42ce65286cfdf0f79184ac8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-11-27 09:26:26 +0100

    tiny

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -414,9 +414,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         loss_mse = loss_terms["loss"].mean()
 
-        norm_factor = code.detach().square().mean()
-        self.norm_factor[:] = (1 - self.momentum) * self.norm_factor \
-                              + self.momentum * norm_factor
+        #norm_factor = code.detach().square().mean()
+        #self.norm_factor[:] = (1 - self.momentum) * self.norm_factor \
+        #                      + self.momentum * norm_factor
         #if global_step <= 1700:
         #    warmup_factor = (2.0 - (math.cos(math.pi * global_step / 1700) + 1.0)) / 2.0
         #else:
