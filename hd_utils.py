f5d0f90982c934c3c280ff0c7bceede33ab9e3b7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-02 10:42:50 +0100

    full parallel

--- hd_utils.py
+++ hd_utils.py
@@ -156,6 +156,7 @@ def generate_big_mlp_from_weights(weights, mlp, batch_size):
     weight_names = list(state_dict.keys())
     for layer in weight_names:
         val = state_dict[layer]
+        print('!!!  ', np.product(list(val.shape)))
         num_params = int(np.product(list(val.shape)) // batch_size)
         w = []
         for i in range(len(weights)):
