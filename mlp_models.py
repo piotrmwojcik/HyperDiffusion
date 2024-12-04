e5a6866f77c6b37d88cc734f438b6222f6141506
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-04 14:15:59 +0100

    tiny_100k

--- mlp_models.py
+++ mlp_models.py
@@ -176,7 +176,7 @@ class ImplicitMLP(nn.Module):
 
 class ImplicitMLPShort(nn.Module):
     def __init__(self, B):
-        super(ImplicitMLP, self).__init__()
+        super(ImplicitMLPShort, self).__init__()
         self.linear1 = nn.Linear(256, 256) #FMMLinear(256, 256, 100)
         self.linear2 = nn.Linear(256, 128) #FMMLinear(256, 128, 50)
         self.linear3 = nn.Linear(128, 32)
@@ -199,7 +199,6 @@ class ImplicitMLPShort(nn.Module):
         output = self.linear5(x).unsqueeze(0)
 
         return {'model_in': coords_org, 'model_out': output}
-
 
 
 class ParallelImplicitMLP(nn.Module):
