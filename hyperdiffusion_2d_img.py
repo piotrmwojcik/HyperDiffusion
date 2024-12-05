947a10e6511e4578a577f35bfc50248b538300b3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-05 21:34:38 +0100

    test parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -538,7 +538,9 @@ class HyperDiffusion_2d_img(torch.nn.Module):
 
         # Initialize a list to store the reshaped images
         images = torch.cat(images, dim=0)
-        print('!!! ', images.shape, image_tensors.shape)
+        flattened_images = images.permute(0, 2, 3, 1).view(images.size(0), -1)
+        flattened_gt = images.view(images.size(0), -1)
+        print('!!! ', flattened_images.shape, flattened_gt.shape)
 
         dot_products = torch.mm(images, images.t())  # Shape: [N, N]
         norms = images.pow(2).sum(dim=1, keepdim=True)  # Shape: [N, 1]
