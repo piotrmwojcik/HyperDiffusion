df428a8ab5993468e28c58c0064e63d8e7b61863
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-05 18:06:58 +0100

    test parallel

--- mlp_models.py
+++ mlp_models.py
@@ -128,7 +128,7 @@ class GaussianFourierFeatureTransform(nn.Module):
         # From [B, C, W, H] to [(B*W*H), C].
         x = x.permute(0, 2, 3, 1).reshape(batches * width * height, channels).to(x.device)
 
-        x = x @ self._B.to(x.device) * 10
+        x = x @ self._B.to(x.device)
 
         # From [(B*W*H), C] to [B, W, H, C]
         x = x.view(batches, width, height, self.mapping_dim)
