a25b6cbac6b488103ffb10993dbe738f021adba4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-06 16:24:33 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -329,7 +329,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         for inverse_step_id in range(n_inverse_steps):
             #psnr = []
             print('!!! ', x.shape)
-            mse_loss, psnr = mlp(x.clone(), gt_imgs)
+            mse_loss, psnr = mlp(x.clone(), gt_imgs.clone())
             print('!!! ', mse_loss.shape)
             #torch.cuda.synchronize()
             #start = time.time()
