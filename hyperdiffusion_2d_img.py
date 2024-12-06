0251b88b008abe67a6072fa6c825fe5e54b0d386
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-06 15:44:24 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -25,7 +25,7 @@ from diffusion.gaussian_diffusion import (GaussianDiffusion, LossType,
                                           ModelMeanType, ModelVarType)
 from ema import ExponentialMovingAverage
 from hd_utils import (Config, calculate_fid_3d, generate_mlp_from_weights,
-                      render_mesh, render_meshes, image_mse)
+                      render_mesh, render_meshes)
 from mlp_models import ImplicitMLP, ParallelImplicitMLP, GaussianFourierFeatureTransform, ImplicitMLPShort, \
     ParallelImplicitShortMLP, image_psnr
 from reg_loss import RegLoss
