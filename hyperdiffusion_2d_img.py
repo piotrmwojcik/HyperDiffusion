e7ba277635b293b401d199bc07093f66c9387f6b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-08 19:25:35 +0100

    3 GPus

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -355,8 +355,8 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 grad_inner = torch.autograd.grad(
                     mse_loss,
                     mlp.parameters(),
-                    create_graph=True,
-                    retain_graph=True
+                    create_graph=False,
+                    retain_graph=False
                 )
 
                 prior_grad_ = torch.cat(prior_grad, dim=0).cuda()
