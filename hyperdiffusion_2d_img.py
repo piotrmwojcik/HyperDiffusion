3ca784dcf651313523e8e139897bab0539bceee5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-08 15:24:13 +0100

    parallel

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -332,13 +332,13 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             outputs = mlp(x.clone())
             #torch.cuda.synchronize()
             #start = time.time()
-            num_gpus = torch.cuda.device_count()
-            chunk_size = outputs.size(0) // num_gpus
-            chunks = torch.split(outputs, chunk_size, dim=0)
-            processed_chunks = []  # List to store processed chunks
-            for chunk in chunks:
-                processed_chunks.append(chunk)
-            outputs = torch.cat(processed_chunks, dim=1)
+            # num_gpus = torch.cuda.device_count()
+            # chunk_size = outputs.size(0) // num_gpus
+            # chunks = torch.split(outputs, chunk_size, dim=0)
+            # processed_chunks = []  # List to store processed chunks
+            # for chunk in chunks:
+            #     processed_chunks.append(chunk)
+            # outputs = torch.cat(processed_chunks, dim=1)
 
             mse_loss = image_mse(mask=None, model_output=outputs, gt=gt_imgs)['img_loss']
             mse_loss = mse_loss * Config.get('code_loss_weight')
