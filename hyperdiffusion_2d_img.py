1cbcf60c6af6a79ff2b09524a7bb75d882910746
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-11 13:52:25 +0100

    big_15

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -337,8 +337,8 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             #    code_reg = self.reg_loss(mlp)
                 #print(self.reg_loss(mlp))
             #    mse_loss = mse_loss + code_reg
-
-            psnr = 5.0 #image_psnr(output['model_out'], gt_imgs)['img_psnr']
+            with torch.no_grad():
+                psnr = image_psnr(output['model_out'], gt_imgs)['img_psnr']
             #psnr.append(psnr_inner)
             #code_optimizer.zero_grad()
             if update_grad:
