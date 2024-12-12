85cbd6700e713225afcb665a3dd45d2d97d16099
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-12 23:17:48 +0100

    norm again

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -444,7 +444,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
             self.norm_factor[:] = (1 - self.momentum) * self.norm_factor \
                                     + self.momentum * norm_factor
         loss_mse = Config.get("loss_weight") * loss_mse / self.norm_factor
-        print('!!!!', Config.get("loss_weight") * self.norm_factor)
+        print('!!!!', Config.get("loss_weight"),  1 / self.norm_factor)
 
         loss_mse.backward()  # Backpropagation
         optimizer.step()
