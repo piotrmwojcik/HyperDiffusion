3dcf23c8fd652d6adcf12a55e2b38e3f3996d653
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-14 23:37:34 +0100

    norm again

--- main_1stage.py
+++ main_1stage.py
@@ -272,6 +272,7 @@ def main(cfg: DictConfig):
 
         #del checkpoint['code_optimizer']['param_groups']
         code_optimizer.state_dict()['state'] = checkpoint['code_optimizer']['state']
+        print(checkpoint['code_optimizer']['state'])
         print(code_optimizer.state_dict()['state'])
         print('Loaded code optimizer')
 
