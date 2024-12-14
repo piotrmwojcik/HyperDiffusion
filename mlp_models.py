5e004475c8fe66b2f5cfd0411afee4a1f4dab775
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-15 00:02:03 +0100

    norm again

--- mlp_models.py
+++ mlp_models.py
@@ -179,7 +179,7 @@ class ImplicitMLP(nn.Module):
 class ImplicitMLPShort(nn.Module):
     def __init__(self):
         super(ImplicitMLPShort, self).__init__()
-        self.linear1 = nn.Linear(256, 256) #FMMLinear(256, 256, 100)
+        self.linear1 = nn.Linear(128, 256) #FMMLinear(256, 256, 100)
         self.linear2 = nn.Linear(256, 128) #FMMLinear(256, 128, 50)
         self.linear3 = nn.Linear(128, 32)
         self.linear4 = nn.Linear(32, 16)
