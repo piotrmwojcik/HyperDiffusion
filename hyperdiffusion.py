3915149c2306de898f4d09ea3b389ba45143a90c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-14 21:50:34 +0100

    norm again

--- hyperdiffusion.py
+++ hyperdiffusion.py
@@ -2,10 +2,8 @@ import copy
 import os
 
 import numpy as np
-import pytorch_lightning as pl
 import torch
 import trimesh
-from pytorch_lightning.utilities.types import EPOCH_OUTPUT
 from scipy.spatial.transform import Rotation
 from tqdm import tqdm
 
