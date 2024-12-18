729dee3c960be23eabdb52f0cc32aa2d0359c5ba
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-18 10:28:22 +0100

    norm again

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -323,6 +323,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         for inverse_step_id in range(n_inverse_steps):
             #psnr = []
             output = mlp(coords)
+            print('!!! ', coords.dtype)
             #print(output['model_out'].shape)
             #start = time.time()
             mse_loss = image_mse(mask=None, model_output=output, gt=gt_imgs)['img_loss']
