333a7a7c37aff5e39cefb4d1944eb4780452d6bc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-12-30 15:09:40 +0100

    debug

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -348,7 +348,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                 #print(f"Number of parameters considered for gradient computation: {num_params}")
 
                 prior_grad_ = torch.cat(prior_grad, dim=0).cuda()
-                prior_grad_norm = prior_grad_.norm().item()
+                #prior_grad_norm = prior_grad_.norm().item()
                 #print('!!! ', prior_grad_.shape)
                 #print('!!! ', grad_inner[0].shape)
                 #for code_idx, single_mlp in enumerate(mlp.models):
@@ -359,7 +359,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
                     grad_shape = grad.shape
                     num_params = np.product(list(grad.shape))
                     grad = grad.view(-1)
-                    print(grad.norm(p=2).item(), prior_grad_[current_idx:current_idx + num_params].norm(p=2).item())
+                    print(grad.shape, grad.norm(p=2).item(), prior_grad_[current_idx:current_idx + num_params].norm(p=2).item())
                     grad = grad + prior_grad_[current_idx:current_idx + num_params]
                     grad = grad.view(grad_shape)
                     param.grad = torch.zeros_like(param).cuda()
