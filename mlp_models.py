c25a58c85e31aa1e3d4dea822150a6ed520ccf2b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-13 19:42:02 +0100

    latent embdding

--- mlp_models.py
+++ mlp_models.py
@@ -149,11 +149,11 @@ class ImplicitMLP(nn.Module):
     def __init__(self, B):
         super(ImplicitMLP, self).__init__()
         self.gff = GaussianFourierFeatureTransform(B=B, mapping_dim=128)
-        self.linear1 = nn.Linear(256, 256) #FMMLinear(256, 256, 100)
-        self.linear2 = nn.Linear(256, 128) #FMMLinear(256, 128, 50)
-        self.linear3 = nn.Linear(128, 32)
-        self.linear4 = nn.Linear(32, 16)
-        self.linear5 = nn.Linear(16, 3)
+        self.linear1 = nn.Linear(256, 128) #FMMLinear(256, 256, 100)
+        self.linear2 = nn.Linear(128, 64) #FMMLinear(256, 128, 50)
+        self.linear3 = nn.Linear(64, 16)
+        self.linear4 = nn.Linear(16, 8)
+        self.linear5 = nn.Linear(8, 3)
 
     def forward(self, model_input):
 
@@ -177,11 +177,11 @@ class ImplicitMLP(nn.Module):
 class ImplicitMLPShort(nn.Module):
     def __init__(self):
         super(ImplicitMLPShort, self).__init__()
-        self.linear1 = nn.Linear(256, 256) #FMMLinear(256, 256, 100)
-        self.linear2 = nn.Linear(256, 128) #FMMLinear(256, 128, 50)
-        self.linear3 = nn.Linear(128, 32)
-        self.linear4 = nn.Linear(32, 16)
-        self.linear5 = nn.Linear(16, 3)
+        self.linear1 = nn.Linear(256, 128) #FMMLinear(256, 256, 100)
+        self.linear2 = nn.Linear(128, 64) #FMMLinear(256, 128, 50)
+        self.linear3 = nn.Linear(64, 16)
+        self.linear4 = nn.Linear(16, 8)
+        self.linear5 = nn.Linear(8, 3)
 
 
     def forward(self, model_input):
