64a403e2911652b7069ee4c9b28f535dda094ac8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-15 17:57:58 +0100

    latent embdding

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -603,11 +603,11 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         siren = generate_mlp_from_weights(weights, self.mlp_kwargs, self.loaded_B)
         #print(self.mlp_kwargs.model_type)
 
-        input = get_grid(64, 64, b=0).unsqueeze(0)
+        input = get_grid(32, 32, b=0).unsqueeze(0)
         result = siren({'coords': input})
         print(result['model_out'].shape)
         # print(img)
-        img = dataio.lin2img(result['model_out'], (64, 64))
+        img = dataio.lin2img(result['model_out'], (32, 32))
         img = dataio.rescale_img((img + 1) / 2, mode='clamp')
         img = (img * 255).byte()
         #print(img)
