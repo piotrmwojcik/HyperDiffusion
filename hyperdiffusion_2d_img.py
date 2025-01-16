9bced13371cc3d3e6cc8522d44dd5d96b9d7c627
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-16 15:39:05 +0100

    latent embdding

--- hyperdiffusion_2d_img.py
+++ hyperdiffusion_2d_img.py
@@ -442,7 +442,7 @@ class HyperDiffusion_2d_img(torch.nn.Module):
         if epoch < Config.get("freeze_norm"):
             self.norm_factor[:] = (1 - self.momentum) * self.norm_factor \
                                     + self.momentum * norm_factor
-        loss_mse = Config.get("loss_weight") * loss_mse #/ self.norm_factor
+        loss_mse = Config.get("loss_weight") * loss_mse / (self.norm_factor / 333.37)
         self.logger.log({"global_step": global_step, "norm factor": (1.0 / (self.norm_factor.item()) / 333.37)})
         #print('!!!!', Config.get("loss_weight"),  1 / self.norm_factor)
 
