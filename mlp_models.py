a2ae1f6f4ee25a9e67a16a1a7a8d799d52b78e6e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-19 17:20:38 +0100

    added plane example

--- mlp_models.py
+++ mlp_models.py
@@ -272,7 +272,7 @@ class MLP3D(nn.Module):
         x = self.layers[-1](x)
 
         if self.output_type == "occ":
-            #x = torch.sigmoid(x)
+            x = torch.sigmoid(x)
             pass
         elif self.output_type == "sdf":
             x = torch.tanh(x)
@@ -280,7 +280,7 @@ class MLP3D(nn.Module):
             x = x
         else:
             raise f"This self.output_type ({self.output_type}) not implemented"
-        x = dist.Bernoulli(logits=x).logits
+        #x = dist.Bernoulli(logits=x).logits
 
         return {"model_in": coords_org, "model_out": x}
 
