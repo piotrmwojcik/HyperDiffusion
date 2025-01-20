115adb18b100e5fbb45ff1172b322bfc5e558208
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 18:23:27 +0100

    added plane example

--- mlp_models.py
+++ mlp_models.py
@@ -281,7 +281,7 @@ class MLP3D(nn.Module):
         else:
             raise f"This self.output_type ({self.output_type}) not implemented"
         x = dist.Bernoulli(logits=x).logits
-        print('!!! ', x.shape, x)
+        #print('!!! ', x.shape, x)
 
         return {"model_in": coords_org, "model_out": x}
 
