3073e47aec3ae44a1d96164fca2cb90dd53c8b0c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 12:52:32 +0100

    added plane example

--- siren/dataio.py
+++ siren/dataio.py
@@ -568,7 +568,7 @@ class PointCloud(Dataset):
                 #print(points.shape, occupancies.shape, occupancies.sum())
                 point_cloud = points
                 print(point_cloud.shape, sdf_values.shape)
-                point_cloud = np.hstack((point_cloud, sdf_values))
+                point_cloud = np.hstack((point_cloud, sdf_values[..., None]))
                 #print(point_cloud.shape, points.shape, occupancies.shape)
 
         else:
