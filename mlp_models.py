de6109cdbbe0c1ad193afa6f27cb19e55c8d89e8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 22:29:57 +0100

    added plane example

--- mlp_models.py
+++ mlp_models.py
@@ -268,8 +268,8 @@ class MLP3D(nn.Module):
         x = self.embedder.embed(x)
         for i, layer in enumerate(self.layers[:-1]):
             x = layer(x)
-            #x = torch.tanh(x)
-            x = F.leaky_relu(x) if self.use_leaky_relu else F.relu(x)
+            x = torch.tanh(x)
+            #x = F.leaky_relu(x) if self.use_leaky_relu else F.relu(x)
         x = self.layers[-1](x)
 
         print(x)
