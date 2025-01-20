46a92bf8721d2e11953b99f1dd931e7750933f33
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 19:17:23 +0100

    added plane example

--- siren/dataio.py
+++ siren/dataio.py
@@ -552,14 +552,12 @@ class PointCloud(Dataset):
                 #query_points = np.random.uniform(-0.5, 0.5, size=(1000, 3))  # Replace with desired points
 
                 # Compute the signed distance values
-                sdf_values, _, normals = igl.signed_distance(
+                sdf_values, _, _, normals = igl.signed_distance(
                     points,
                     obj.vertices,
                     obj.faces,
-                    return_normals=False  # Set to True if normals are needed
+                    return_normals=True  # Set to True if normals are needed
                 )
-
-                print('!!! ', np.min(sdf_values), np.max(sdf_values))
 
                 #normals = np.array(obj.face_normals[closest_faces])
 
