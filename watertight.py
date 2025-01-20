6b9f96824df5aed82244fa7798eaf25997c80b16
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 21:34:06 +0100

    added plane example

--- watertight.py
+++ watertight.py
@@ -29,6 +29,9 @@ p_vol = (np.random.rand(num_vol_pts, 3) - 0.5) * 1.1
 sdf, _, _  = pcu.signed_distance_to_mesh(p_vol, vm, fm)
 print(sdf.shape)
 
+output = np.hstack(p_vol, sdf[..., None])
+np.save('dupa.npy', output)
+
 # Sample points on the surface as face ids and barycentric coordinates
 #fid_surf, bc_surf = pcu.sample_mesh_random(vm, fm, num_surf_pts)
 
