96a31950cd9000072a87d44c2cd2e48cd825e7fb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 18:55:34 +0100

    added plane example

--- siren/loss_functions.py
+++ siren/loss_functions.py
@@ -326,7 +326,7 @@ def occ_sigmoid(model_output, gt, model, cfg=None, first_state_dict=None):
 def occ_tanh(model_output, gt, model, cfg=None):
     gt_sdf = gt["sdf"]
     pred_sdf = model_output["model_out"]
-    print(gt_sdf, pred_sdf)
+    #print(gt_sdf, pred_sdf)
 
     # kl_loss_fn = torchbnn.BKLLoss(reduction='mean', last_layer_only=False)
     # kl_loss = kl_loss_fn(model)
