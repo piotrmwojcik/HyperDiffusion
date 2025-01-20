6d5e126b995c8a26977018fdcd53f4b83408c387
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-01-20 19:19:33 +0100

    added plane example

--- siren/sdf_meshing.py
+++ siren/sdf_meshing.py
@@ -242,6 +242,7 @@ def convert_sdf_samples_to_ply(
         np.zeros((0, 3)),
         np.zeros(0),
     )
+    print(numpy_3d_sdf_tensor)
     try:
         verts, faces, normals, values = skimage.measure.marching_cubes_lewiner(
             numpy_3d_sdf_tensor, level=level, spacing=[voxel_size] * 3
